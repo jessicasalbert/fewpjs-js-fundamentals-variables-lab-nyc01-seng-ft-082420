52d3ec14e3fdbbe73ebe23478749c113858ce10f
Jessica Salbert <jessicarsalbert@gmail.com>
2020-10-04 18:17:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
+<<<<<<< HEAD
+=======
+
+>>>>>>> 796a60a15a742d30987f440c8e2531bb372d0eb3
 const companyName = "Scuber";
 
 let mostProfitableNeighborhood = "Chelsea";
